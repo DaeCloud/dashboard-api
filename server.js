67ff7b7cfd37a5141bca5a8a4d44ba207b02b1c4
Daelin Campleman <127960675+Daelin-Campleman@users.noreply.github.com>
2025-03-20 16:51:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,23 +8,29 @@ const port = 3000;
 
 app.get("/speed", async (req, res) => {
   const url = process.env.SPEED_DOWN_URL;
+  const numDownloads = process.env.NUM_DOWNLOADS || 5;
 
-  // Measure download speed
-  const startTime = performance.now();
-  const response = await axios.get(url, { responseType: "blob" });
-  const blob = response.data;
-  const endTime = performance.now();
-  const duration = (endTime - startTime) / 1000; // Convert to seconds
+  const downloadPromises = [];
+    const startTime = performance.now();
 
-  const fileSizeMB = process.env.SPEED_FILE_SIZE; // File size in MB
-  const downloadSpeedMbps = (fileSizeMB * 8) / duration; // Calculate speed in Mbps
+    for (let i = 0; i < numDownloads; i++) {
+        downloadPromises.push(axios.get(url, { responseType: "blob" }));
+    }
 
-  console.log(`Download time: ${duration} seconds`);
-  console.log(`Download speed: ${downloadSpeedMbps.toFixed(2)} Mbps`);
+    const responses = await Promise.all(downloadPromises);
+    const blob = responses[0].data;
+    const endTime = performance.now();
+    const duration = (endTime - startTime) / 1000; // Convert to seconds
 
-  res.status(200).json({
-    downloadSpeed: `${downloadSpeedMbps.toFixed(2)}`,
-  });
+    const totalFileSizeMB = fileSizeMB * numDownloads;
+    const downloadSpeedMbps = (totalFileSizeMB * 8) / duration; // Calculate speed in Mbps
+
+    console.log(`Download time: ${duration} seconds`);
+    console.log(`Download speed: ${downloadSpeedMbps.toFixed(2)} Mbps`);
+  
+    res.status(200).json({
+      downloadSpeed: `${downloadSpeedMbps.toFixed(2)}`
+    });
 });
 
 app.listen(port, () => {
